a17b0264d4c43ddbe648b467909133c560043b6f
Ruben <ruben.fernandez@correounivalle.edu.co>
2022-11-02 20:54:27 -0500

    commit de la rama inicial para desarrollo de modulo de configuracion

--- prueba.py
+++
@@ -1,5 +0,0 @@
-import pyautogui, time
-time.sleep(5)
-currentMouseX, currentMouseY = pyautogui.position() # Get the XY position of the mouse.
-currentMouseX, currentMouseY
-print(currentMouseX,currentMouseY)
